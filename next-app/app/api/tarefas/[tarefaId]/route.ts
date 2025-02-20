c3b8d212ffb1948714669e3a94421f4840984b70
Marcos Gabriel Dias Fernandes <110645752+GambollGabriel@users.noreply.github.com>
2025-02-20 01:50:37 +0000

    Aula etec 19022025

---
+++ next-app/app/api/tarefas/[tarefaId]/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import tasks from '@/dados.json';
+
+export async function GET(request: Request, context: any){
+
+    const {params} = context;
+
+    let resultado = null;
+
+    tasks.forEach((item) =>{
+        if(item.id == params.tarefaId){
+            resultado = item;
+        }
+    });
+
+    return NextResponse.json(resultado)
+}
--- next-app/app/api/tarefas/route.ts
+++ next-app/app/api/tarefas/route.ts
@@ -1,14 +1,17 @@
 import { NextResponse } from "next/server";
 
+let tasks = [
+    {id: 1, title: 'Aprender NextJS'},
+    {id: 2, title: 'Criar API REST'},
+];
+
 export async function GET() {  // é uma função asincrona (não é sincronizado), tem que mandar esperar
-    return NextResponse.json({
-        mensagem: "Olá Next!!!" 
-    });
+    return NextResponse.json(tasks);
 }
 
 
-export async function POST(){
-    return NextResponse .json({
-        mensagem: "Olá NEXT - POST!!!"
-    })
+export async function POST(request: Request){
+    const data = await request.json()
+    tasks.push(data)
+    return NextResponse .json(tasks);
 }
